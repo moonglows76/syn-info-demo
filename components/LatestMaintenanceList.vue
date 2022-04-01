8ab7d8684b2781ad96a350cfc5a719f74fd1a808
森史憲 <mori@codingdesing.jp>
2022-04-01 12:09:45 +0900

    メンテナンス計画の上部に「最近のメンテナンス」部分を追加

---
+++ components/LatestMaintenanceList.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="latest-maintenance-list">
+    <li
+      v-for="content in contents"
+      :key="content.id"
+      class="latest-maintenance-list__item"
+    >
+      <NuxtLink :to="`/info/maintenance/posts/${content.id}`">
+        <span
+          :class="content.body[0].category[0] === 'メンテナンス完了' ? 'red' : 'blue'"
+        >
+          {{ content.body[0].category[0] }}
+        </span> {{ content.title }}<br>
+        実施日時：<br>
+        <div>
+          {{ $dayjs(content.body[0].datetime_start).format('YYYY年M月D日(ddd) HH:mm') }} 〜 {{ $dayjs(content.body[0].datetime_end).format('YYYY年M月D日(ddd) HH:mm') }}
+        </div>
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    contents: {
+      type: Array,
+      default: () => [],
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+::v-deep {
+  p {
+    margin: 0;
+  }
+}
+.red {
+  background: red;
+  color: #fff;
+}
+.blue {
+  background: blue;
+  color: #fff;
+}
+</style>
--- pages/info/maintenance/index.vue
+++ pages/info/maintenance/index.vue
@@ -3,6 +3,16 @@
     <h1>メンテナンス計画</h1>
     <p><NuxtLink to="/">トップページ</NuxtLink></p>
     <NavInfo />
+    <h2>直近のメンテナンス</h2>
+    <div v-if="latestContents.length === 0">
+      <p>この３日間に行われるメンテナンスはありません。</p>
+    </div>
+    <div v-else>
+      <LatestMaintenanceList :contents="latestContents" />
+    </div>
+
+    <hr>
+
     <MaintenanceList :contents="contents" />
     <InformationMonthList
       type="maintenance"
@@ -21,11 +31,31 @@ export default {
   async asyncData({ $microcms }) {
     const { contents } = await $microcms.get({
       endpoint: 'info',
-      queries: {filters: 'type[equals]maintenance'},
+      queries: {
+        filters: 'type[equals]maintenance',
+        limit: 20,
+      },
     })
     return {
       contents
     }
-  }
+  },
+  computed: {
+    latestContents() {
+      // 現在時刻を取得
+      const now = this.$dayjs(new Date())
+      // microCMSから取得した投稿情報をフィルタリング
+      const arr = this.contents.filter((content) => {
+        // 投稿情報内のメンテナンス開始日時が現在から3日以内であれば配列arrに入れる
+        // to日時.diff(from日時, 'day')で日数の差分をとる（year, hourなどの指定も可能 https://day.js.org/docs/en/manipulate/subtract）
+        const reslut = this.$dayjs(content.body[0].datetime_start).diff(now, 'day')
+        // 過去ではなく（過去はresultがマイナス値になるため0以上とする）、未来の3日以内
+        if(0 <= reslut && reslut <= 3) {
+          return content
+        }
+      })
+      return arr
+    },
+  },
 }
 </script>
