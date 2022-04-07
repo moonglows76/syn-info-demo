6dc54ebf13bcc65aac4f326a5bf14c65da963a86
森史憲 <mori@codingdesing.jp>
2022-04-07 10:25:20 +0900

    トップページのお知らせ表示件数の制限を追加、障害報告とメンテナンス計画の更新年月表示にAPIを反映

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@
     <h1>トップ</h1>
     <h2>シナプスからのお知らせ</h2>
     <p><NuxtLink to="/info">お知らせ一覧へ</NuxtLink></p>
-    <InformationList :contents="contents" />
+    <InformationList :contents="informationContents" />
     <div>
-      <NuxtLink to="/info/trouble">障害報告 -◯月◯日更新</NuxtLink>
+      <NuxtLink to="/info/trouble">障害報告 -{{ updateDate('trouble') }}更新</NuxtLink>
     </div>
     <div>
-      <NuxtLink to="/info/maintenance">メンテナンス計画 -◯月◯日更新</NuxtLink>
+      <NuxtLink to="/info/maintenance">メンテナンス計画 -{{ updateDate('maintenance') }}更新</NuxtLink>
     </div>
   </div>
 </template>
@@ -24,11 +24,26 @@ export default {
   async asyncData({ $microcms }) {
     const { contents } = await $microcms.get({
       endpoint: 'info',
-      queries: {filters: 'type[equals]information'},
+      queries: { limit: 100 }
     })
     return {
       contents
     }
-  }
+  },
+  computed: {
+    informationContents() {
+      const limit = 10
+      return this.contents
+        .filter(content => content.type.id === 'information')
+        .slice(0, limit)
+    },
+  },
+  methods: {
+    updateDate(type) {
+      const articles = this.contents.filter(content => content.type.id === type)
+      // return '4月4日'
+      return this.$dayjs(articles[0].publishedAt).format('M月D日')
+    },
+  },
 }
 </script>
